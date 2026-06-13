174eb15cc22bb255e672f96f008d1d5dc3706d16
EJ Mason <13525251+mxmason@users.noreply.github.com>
2026-06-13 13:50:24 -0700

    feat(client): set up routing, homepage

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-import { App } from "./App";
+import { Home } from "./pages/Home";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route element={<Home />} index />
+			</Routes>
+		</BrowserRouter>
 	</StrictMode>,
 );
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-export function App() {
+export function Home() {
 	React.useEffect(() => {
 		fetch("/api/hello/World")
 			.then((res) => res.json())
