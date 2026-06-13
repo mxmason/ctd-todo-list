bff9e05a5c3ee1a7b9008b658216a6810ae5a73f
EJ Mason <13525251+mxmason@users.noreply.github.com>
2026-06-13 13:10:50 -0700

    feat(client): add more api helpers

--- client/src/api/todos.ts
+++ client/src/api/todos.ts
@@ -3,3 +3,20 @@ import { type Todo } from "#shared";
 import { apiFetch } from "./utils.ts";
 
 export const getTodos = () => apiFetch<Todo[]>("/todos");
+
+export const createTodo = (title: string) =>
+	apiFetch<Todo>("/todos", {
+		method: "POST",
+		body: JSON.stringify({ title }),
+	});
+
+export const setTodoCompleted = (id: string, completed: boolean) =>
+	apiFetch<Todo>(`/todos/${id}`, {
+		method: "PATCH",
+		body: JSON.stringify({ completed }),
+	});
+
+export const deleteTodo = (id: string) =>
+	apiFetch(`/todos/${id}`, {
+		method: "DELETE",
+	});
