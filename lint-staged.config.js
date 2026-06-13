592b1a1410685949754e4fc7c1dd5952020db277
EJ Mason <13525251+mxmason@users.noreply.github.com>
2026-06-13 12:51:26 -0700

    feat(dx): update tsconfigs

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -11,6 +11,9 @@ export default {
 		() => "node_modules/.bin/tsgo -b client/tsconfig.json",
 		"eslint --fix",
 	],
-	"shared/src/**/*.{ts,tsx}": ["eslint --fix"],
+	"shared/src/**/*.{ts,tsx}": [
+		() => "node_modules/.bin/tsgo -b shared/tsconfig.json",
+		"eslint --fix",
+	],
 	"**/*.{css,json,js,ts,tsx}": ["prettier --write"],
 };
