410605c448b97a0770db93bf8c11b7fde9e64a6b
EJ Mason <13525251+mxmason@users.noreply.github.com>
2026-06-16 09:11:46 -0700

    feat(client): refactor router with protected route, new pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,18 +2,28 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 
-import { Layout } from "./layouts/Layout";
-import { Home } from "./pages/Home";
+import { ProtectedRoute } from "./components/ProtectedRoute.tsx";
+import { AuthProvider } from "./context/auth/index.ts";
+import { Layout } from "./layouts/Layout.tsx";
+import { Home } from "./pages/Home.tsx";
+import { Login } from "./pages/Login.tsx";
+import { Register } from "./pages/Register.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
-			<Routes>
-				<Route element={<Layout />}>
-					<Route element={<Home />} index />
-					<Route element={<p>Page not found</p>} path="*" />
-				</Route>
-			</Routes>
+			<AuthProvider>
+				<Routes>
+					<Route element={<Layout />}>
+						<Route element={<Login />} path="/login" />
+						<Route element={<Register />} path="/register" />
+						<Route element={<ProtectedRoute />}>
+							<Route element={<Home />} index />
+						</Route>
+						<Route element={<p>Page not found</p>} path="*" />
+					</Route>
+				</Routes>
+			</AuthProvider>
 		</BrowserRouter>
 	</StrictMode>,
 );
