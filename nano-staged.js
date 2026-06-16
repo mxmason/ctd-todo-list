6a5f4a3c88cdd46284d1b5a9bb630964f3707350
EJ Mason <13525251+mxmason@users.noreply.github.com>
2026-06-15 20:43:37 -0700

    chore(dx): replace lint-staged with nano-staged

---
+++ nano-staged.js
@@ -0,0 +1,26 @@
+/**
+ * @filename: nano-staged.js}
+  @import {FnConfiguration, ObjConfiguration} from 'nano-staged'
+ */
+
+/**
+ * @param {Parameters<FnConfiguration>[0]} api
+ */
+const eslint = ({ filenames }) => `eslint --cache --fix ${filenames.join(" ")}`;
+
+/** @type {ObjConfiguration} */
+export default {
+	"server/src/**/*.{ts,tsx}": (api) => [
+		"tsgo -b server/tsconfig.json",
+		eslint(api),
+	],
+	"client/src/**/*.{ts,tsx}": (api) => [
+		"tsgo -b client/tsconfig.json",
+		eslint(api),
+	],
+	"shared/src/**/*.{ts,tsx}": (api) => [
+		"tsgo -b shared/tsconfig.json",
+		eslint(api),
+	],
+	"**/*.{css,json,js,ts,tsx}": ["prettier --write"],
+};
