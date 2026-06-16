a664d2f52d7880adf05d7b6955a04a38bdf8ef03
EJ Mason <13525251+mxmason@users.noreply.github.com>
2026-06-16 11:53:39 -0700

    chore: remove fetch from homepage

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,3 @@
-import * as React from "react";
-
 export function Home() {
-	React.useEffect(() => {
-		fetch("/api/hello/World")
-			.then((res) => res.json())
-			.then((data) => console.log(data));
-	}, []);
 	return <h1>Hello world</h1>;
 }
