74e541fb41e09420384399b837d5b51abb2c0580
EJ Mason <13525251+mxmason@users.noreply.github.com>
2026-06-16 17:11:14 -0700

    feat(db): update migration

--- server/prisma/migrations/20260617000946_init/migration.sql
+++ server/prisma/migrations/20260617000946_init/migration.sql
@@ -2,7 +2,9 @@
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
     "username" TEXT NOT NULL,
-    "password_hash" TEXT NOT NULL,
+    "password_hash" TEXT,
+    "email" TEXT,
+    "google_id" TEXT,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
@@ -20,6 +22,12 @@ CREATE TABLE "todos" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_google_id_key" ON "users"("google_id");
 
 -- CreateIndex
 CREATE INDEX "todos_user_id_idx" ON "todos"("user_id");
