022d1c35c4af201b9d4954031bccefde622aa633
EJ Mason <13525251+mxmason@users.noreply.github.com>
2026-06-22 09:58:05 -0700

    chore(dx): update lint scripts for typescript@rc1

--- nano-staged.js
+++ nano-staged.js
@@ -11,15 +11,15 @@ const eslint = ({ filenames }) => `eslint --cache --fix ${filenames.join(" ")}`;
 /** @type {ObjConfiguration} */
 export default {
 	"server/src/**/*.{ts,tsx}": (api) => [
-		"tsgo -b server/tsconfig.json",
+		"tsc -b server/tsconfig.json",
 		eslint(api),
 	],
 	"client/src/**/*.{ts,tsx}": (api) => [
-		"tsgo -b client/tsconfig.json",
+		"tsc -b client/tsconfig.json",
 		eslint(api),
 	],
 	"shared/src/**/*.{ts,tsx}": (api) => [
-		"tsgo -b shared/tsconfig.json",
+		"tsc -b shared/tsconfig.json",
 		eslint(api),
 	],
 	"**/*.{css,json,js,ts,tsx}": ["prettier --write"],
